e821d69061d58d5c8e0c72bcf72db5d03d581138
pyohogeun <user@users-MacBook-Pro.local>
2024-01-04 09:50:08 +0900

    layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('연결');
+$(document).ready(function(){
+  console.log('연결');
+  getSectionHeight();
+  getOffset();
+  console.log(sectionOffArr);
+});
+let sectionOffArr = [];
+function getSectionHeight(){
+  $section = $('.section').each(function(i, e){
+    let offset = parseInt($(this).offset().top);
+    sectionOffArr.push(offset);
+  });
+}
+function getOffset(){
+  let timer;
+  $(window).on('scroll', function(e){
+    if(!timer){
+      timer = setTimeout(() => {
+        timer = null;
+        let top = $(this).scrollTop();
+        console.log('scroll', parseInt(top));
+      }, 300);
+    }
+  })
+}
